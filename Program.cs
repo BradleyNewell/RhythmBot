18a675f1ff345556a78680771e7598208a6b0a2e
[Bradley] <[40966735+BradleyXVIII@users.noreply.github.com]>
2020-04-30 08:41:02 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RhythmBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            string[] strum =  {"U", "D", "_"};
+            List<string> pattern = new List<string>();
+
+            
+            for (int i = 0; i <= 8; i++)
+            {
+                int randstrum = rnd.Next(strum.Length);
+                pattern.Add(strum[randstrum]);
+            }
+            foreach (string i in pattern)
+            {
+                Console.Write(i);
+            }
+            
+        }
+    }
+}
